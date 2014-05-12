51c6337d45afc1f504d040bbdbabf17333e25bc1
Joe Morris <joem@joemdesign.com>
2014-05-12 11:30:26 -0400

    Added Console .h & .m

--- goingThruNotes/main.m
+++ goingThruNotes/main.m
@@ -8,13 +8,27 @@
 
 #import <Foundation/Foundation.h>
 
+void myFunction(NSString *x, int y, int a, int b){
+    NSLog(@"My name is %@ and I am %i years old", x, y);
+    NSLog(@"In %i years I will be %i" a, b);
+}
+
 int main(int argc, const char * argv[])
 {
+//    void myExpression (int x1, int x2){
+//        
+//    }
+    
 
+    
     @autoreleasepool {
+        NSString *name = @"Joe";
+        int age = 46;
+        int time = userInput;
+        int newAge = age + time;
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        myFunction(name, age, time, newAge);
+        
         
     }
     return 0;
